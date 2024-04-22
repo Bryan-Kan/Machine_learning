30014a0698d89f02d35b1cabb8bbdc6af3125310
Bryan-Kan <142849196+Bryan-Kan@users.noreply.github.com>
2024-04-22 15:17:09 +0000

    escala

--- app.js
+++ app.js
@@ -15,25 +15,28 @@ function dados() {
 
             const qtdAparelho = iphone[i].qtd;
 
-            if(qtdAparelho == iphone[0].qtd) {
+            let x = qtdAparelho/iphone[0].qtd
+
+            if(x <= 1 && x >= 0,90) {
                 console.log("Excelente");
                 alert("Excelente", "")
-            } else if(qtdAparelho == iphone[1].qtd) {
-                console.log("Bom");
-                alert("Bom", "")
-            } else if(qtdAparelho == iphone[2].qtd) {
-                console.log("Razoável");
-                alert("Razoável", "")
-            }else if(qtdAparelho == iphone[3].qtd) {
-                console.log("Ruim");
-                alert("Ruim", "")
-            }else if(qtdAparelho == iphone[4].qtd) {
-                console.log("Péssimo");
-                alert("Péssimo", "")
-            }else {
-                alert("Não avaliado", "")
-            }
+            } 
+            // else if(qtdAparelho == iphone[1].qtd) {
+            //     console.log("Bom");
+            //     alert("Bom", "")
+            // } else if(qtdAparelho == iphone[2].qtd) {
+            //     console.log("Razoável");
+            //     alert("Razoável", "")
+            // }else if(qtdAparelho == iphone[3].qtd) {
+            //     console.log("Ruim");
+            //     alert("Ruim", "")
+            // }else if(qtdAparelho == iphone[4].qtd) {
+            //     console.log("Péssimo");
+            //     alert("Péssimo", "")
+            // }
 
+        }else{
+            alert("Não avaliado", "")
         }
 
     }     
